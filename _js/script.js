f1edde7941f6c9b2b0037aeb263eb883fd70e8f0
Hassan Jannah <hassan.jannah@gmail.com>
2016-03-09 07:41:56 +0300

    Update script.js

--- _js/script.js
+++ _js/script.js
@@ -560,11 +560,11 @@ function refreshNavigation() {
 function loadTemplates(templates, templatesURL) {
     //    console.log('loading templates ' + templates.length)
     templatesURL = (templatesURL) ? templatesURL : '';
-    for (var template in templates) {
+    for (var tmpl in templates) {
         // console.log(template);
-        templates[template]['url'] = templatesURL + '/' + templates[template].filename;
-        templates[template]['html'] = loadTemplate(templates[template].url);
-        templates[template]['render'] = _.template(templates[template]['html']);
+        templates[tmpl]['url'] = templatesURL + '/' + templates[tmpl].filename;
+        templates[tmpl]['html'] = loadTemplate(templates[tmpl].url);
+        templates[tmpl]['render'] = _.template(templates[tmpl]['html']);
     }
     return templates;
 }
